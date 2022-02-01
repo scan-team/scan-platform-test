b70cb9081053d54190c1c021a7ed772ed361e1a5
Jun Fujima <jfujima@gmail.com>
2022-02-01 23:16:15 +0900

    add API_WORKERS env val

--- scan_api_internal/gunicorn.conf.py
+++ scan_api_internal/gunicorn.conf.py
@@ -1,9 +1,11 @@
 import os
 
+API_WORKERS = os.environ.get("API_WORKERS", os.cpu_count() + 1)
+
 bind = '0.0.0.0:8000'
 
 # Worker Processes
-workers = 2 * os.cpu_count() + 1
+workers = API_WORKERS
 threads = 2 * os.cpu_count() + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 
