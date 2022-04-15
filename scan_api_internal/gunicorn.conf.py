a54a4d88d25fa63115ac565b1453ef0052a35281
Jun Fujima <jf.devvv@gmail.com>
2022-04-16 01:21:25 +0900

    fix api worker process count

--- scan_api_internal/gunicorn.conf.py
+++ scan_api_internal/gunicorn.conf.py
@@ -1,12 +1,12 @@
 import os
 
-API_WORKERS = os.environ.get("API_WORKERS", os.cpu_count() + 1)
+API_WORKERS = os.environ.get("API_WORKERS", 2 * os.cpu_count() + 1)
 
 bind = '0.0.0.0:8000'
 
 # Worker Processes
 workers = API_WORKERS
-threads = 2 * os.cpu_count() + 1
+threads = 2
 worker_class = 'uvicorn.workers.UvicornWorker'
 
 print("workers: ", workers)
