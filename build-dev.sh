db4bfb9612c47934f3adc380b8f1c87a78f6ba24
asterisoft-saito <saito@asterisoft.co.jp>
2022-07-11 02:43:26 +0000

    自動ログのログメッセージを修正&ビルド時のキャッシュの効率化

--- build-dev.sh
+++ build-dev.sh
@@ -1,7 +1,17 @@
-#!/bin/sh
-docker build ./scan-mariadb/docker/db -t scan-mariadb
-docker build ./scan_api_internal -t scan_api_internal -f ./scan-app/Dockerfile-dev
-docker build ./scan_api_public -t scan_api_public -f ./scan-app/Dockerfile-dev
-docker build ./scan-app -t scan-app -f ./scan-app/Dockerfile-dev --build-arg NEXT_PUBLIC_SCAN_API_PROXY_ROOT=${NEXT_PUBLIC_SCAN_API_PROXY_ROOT} --build-arg NEXT_PUBLIC_GOOGLE_ANALYTICS_ID=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}
-docker build ./scan_data_observer -t scan_data_observer 
-docker build ./scan_data_observer/scan_data_importer -t scan_data_importer
+#!/bin/bash
+
+while (( $# > 0 ))
+do
+  case $1 in
+    --no-cache)
+      NO_CACHE_OPTION=$1
+  esac
+  shift
+done
+
+docker build $NO_CACHE_OPTION ./scan-mariadb/docker/db -t scan-mariadb
+docker build $NO_CACHE_OPTION ./scan_api_internal -t scan_api_internal-dev -f ./scan_api_internal/Dockerfile-dev
+docker build $NO_CACHE_OPTION ./scan_api_public -t scan_api_public-dev -f ./scan_api_public/Dockerfile-dev
+docker build $NO_CACHE_OPTION ./scan-app -t scan-app-dev -f ./scan-app/Dockerfile-dev --build-arg NEXT_PUBLIC_SCAN_API_PROXY_ROOT=${NEXT_PUBLIC_SCAN_API_PROXY_ROOT} --build-arg NEXT_PUBLIC_GOOGLE_ANALYTICS_ID=${NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}
+docker build $NO_CACHE_OPTION ./scan_data_observer -t scan_data_observer 
+docker build $NO_CACHE_OPTION ./scan_data_observer/scan_data_importer -t scan_data_importer
